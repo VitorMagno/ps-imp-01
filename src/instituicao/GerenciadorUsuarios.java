31a0049354b7535f829f7ef222a2e34941b5f5aa
VitorMagno <vmcg@ic.ufal.br>
2022-10-06 09:51:27 -0300

    create a general list to all users

--- src/instituicao/GerenciadorUsuarios.java
+++ src/instituicao/GerenciadorUsuarios.java
@@ -16,7 +16,11 @@ public class GerenciadorUsuarios {
         if(alunos == null) {
             alunos = new ArrayList<Aluno>();
         }
+        if(usuarios == null) {
+            usuarios = new ArrayList<Usuario>();
+        }
         Aluno novoAluno = new Aluno(nome, tipoGraduacao, cpf);
+        usuarios.add(novoAluno);
         alunos.add(novoAluno);
         int indexOfAluno = alunos.lastIndexOf(novoAluno);
         return alunos.get(indexOfAluno);
@@ -25,7 +29,11 @@ public class GerenciadorUsuarios {
         if(pesquisadores == null) {
             pesquisadores = new ArrayList<Pesquisador>();
         }
+        if(usuarios == null) {
+            usuarios = new ArrayList<Usuario>();
+        }
         Pesquisador novoPesquisador = new Pesquisador(nome, tipoGraduacao, cpf);
+        usuarios.add(novoPesquisador);
         pesquisadores.add(novoPesquisador);
         int indexOfPesquisador = pesquisadores.lastIndexOf(novoPesquisador);
         return pesquisadores.get(indexOfPesquisador);
@@ -34,7 +42,11 @@ public class GerenciadorUsuarios {
         if(professores == null) {
             professores = new ArrayList<Professor>();
         }
+        if(usuarios == null) {
+            usuarios = new ArrayList<Usuario>();
+        }
         Professor novoProfessor = new Professor(nome, tipoGraduacao, cpf);
+        usuarios.add(novoProfessor);
         professores.add(novoProfessor);
         int indexOfProfessor = professores.lastIndexOf(novoProfessor);
         return professores.get(indexOfProfessor);
@@ -43,7 +55,11 @@ public class GerenciadorUsuarios {
         if(profissionais == null) {
             profissionais = new ArrayList<Profissional>();
         }
+        if(usuarios == null) {
+            usuarios = new ArrayList<Usuario>();
+        }
         Profissional novoProfissional = new Profissional(nome, tipoGraduacao, cpf);
+        usuarios.add(novoProfissional);
         profissionais.add(novoProfissional);
         int indexOfProfissional = profissionais.lastIndexOf(novoProfissional);
         return profissionais.get(indexOfProfissional);
@@ -52,7 +68,11 @@ public class GerenciadorUsuarios {
         if(tecnicos == null) {
             tecnicos = new ArrayList<Tecnico>();
         }
+        if(usuarios == null) {
+            usuarios = new ArrayList<Usuario>();
+        }
         Tecnico novoTecnico = new Tecnico(nome, tipoGraduacao, cpf);
+        usuarios.add(novoTecnico);
         tecnicos.add(novoTecnico);
         int indexOfTecnico = tecnicos.lastIndexOf(novoTecnico);
         return tecnicos.get(indexOfTecnico);
