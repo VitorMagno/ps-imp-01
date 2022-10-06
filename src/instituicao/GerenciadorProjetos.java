f81b43f304d2e508ae3f6190380e872a1669a1e8
VitorMagno <vmcg@ic.ufal.br>
2022-10-06 09:29:28 -0300

    create new project, at projects and return index

--- src/instituicao/GerenciadorProjetos.java
+++ src/instituicao/GerenciadorProjetos.java
@@ -9,12 +9,16 @@ import src.usuario.*;
 public class GerenciadorProjetos {
     ArrayList<Projeto> projetos;
     private int id=0;
-    public void createProjeto(int id, String identificacao, String descricao, Professor coordenador, int valorBolsa, Date dataHrInicio, Date dataHrTermino, Date vigenciaBolsa) {
+    // cria um projeto, coloca na lista de projetos e retorna o index desse projeto  
+    public Projeto createProjeto(String identificacao, String descricao, Professor coordenador, int valorBolsa, Date dataHrInicio, Date dataHrTermino, Date vigenciaBolsa) {
         if(projetos == null) {
             projetos = new ArrayList<Projeto>();
         }
-        Projeto novoProjeto = new Projeto(id+1, identificacao, descricao, coordenador, valorBolsa, dataHrInicio, dataHrTermino, vigenciaBolsa);
+        this.id = id++;
+        Projeto novoProjeto = new Projeto(this.id, identificacao, descricao, coordenador, valorBolsa, dataHrInicio, dataHrTermino, vigenciaBolsa);
         projetos.add(novoProjeto);
+        int indexOfProjeto = projetos.lastIndexOf(novoProjeto);
+        return projetos.get(indexOfProjeto);
     }
     public void getProjeto(){
         for (Projeto elem: projetos){
